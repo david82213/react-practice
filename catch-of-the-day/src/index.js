4b0ed65870563eaad40706446646541a624f9c47
Daniel Lin <david82213@hotmail.com>
2017-01-24 01:13:24 -0800

    created first component and export to use in index.js

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,11 @@
 // let's go!
+import React from 'react';
+// just import render method from react-dom
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+
+// 1st: render what component
+// 2nd: render to what DOM element
+render(<StorePicker/>, document.querySelector('#main'));
