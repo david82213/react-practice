aa071f44a451ee0148e0a605e954f91bfbd90745
Daniel Lin <david82213@hotmail.com>
2017-01-24 16:47:21 -0800

    adding jsx to return statement instead of using React.createElement

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 class StorePicker extends React.Component{
   render() {
-    return <p>Hello</p>
+    // return React.createElement('p', {className: 'Testing'}, 'Hello');
+    // jsx below
+    return (
+      <form className="store-selector">
+        {/* Comment */}
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store 👉</button>
+      </form>
+    )
   }
 }
 
