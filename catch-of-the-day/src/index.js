2d0e8b7b4c4aae675b664a1ca23b25e95051ce21
Daniel Lin <david82213@hotmail.com>
2017-01-27 00:50:30 -0800

    added react-router routing

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -8,8 +8,24 @@ import './css/style.css';
 
 import App from './components/App';
 
+import { BrowserRouter, Match, Miss } from 'react-router';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
 
 // 1st: render what component
 // 2nd: render to what DOM element
 // render(<StorePicker/>, document.querySelector('#main'));
-render(<App/>, document.querySelector('#main'));
+// render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));
