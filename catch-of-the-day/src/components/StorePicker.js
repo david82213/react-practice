965cb3cc7af63a3ffa6efd4b91947c819f729f88
Daniel Lin <david82213@hotmail.com>
2017-01-29 23:54:00 -0800

    implement obSubmit and ref for form element

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,30 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+  // constructor(){
+  //   // super runs React.Component first then runs custom class and functions
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  goToStore(event){
+    // prevent page from refreshing
+    event.preventDefault();
+
+    // first grab text from input field
+    const value = this.storeInput.value;
+    // second, transition from "/" to "/store/:storeID"
+  }
+
   render() {
     // return React.createElement('p', {className: 'Testing'}, 'Hello');
     // jsx below
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         {/* Comment */}
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        {/* put a reference to a variable which binds to class*/}
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store 👉</button>
       </form>
     )
