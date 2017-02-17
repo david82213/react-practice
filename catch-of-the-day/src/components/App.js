44ea0f7fdd99aa2b06516ba7325d20246fb9e7eb
Daniel Lin <david82213@hotmail.com>
2017-02-16 16:58:17 -0800

    persist order state with localStorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -25,16 +25,32 @@ class App extends React.Component{
 
   // before component gets rendered, sync firebase with it
   // client and server sync together
+  // this runs right before <app> is rendered
   componentWillMount(){
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
+
+    // check if there is any order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if (localStorageRef){
+      // if exists in localStorage, update App component's order state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
   }
 
   // remove listening to changes in states when we change the page
   componentWillUnmount(){
     base.removeBinding(this.ref);
+  }
+
+  // run when new props or state is received
+  componentWillUpdate(nextProps, nextState){
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
   }
 
   addFish(fish){
@@ -83,7 +99,7 @@ class App extends React.Component{
           </ul>
         </div>
 
-        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Order params={this.props.params} fishes={this.state.fishes} order={this.state.order}/>
         {/* passing the function down to child as props */}
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
